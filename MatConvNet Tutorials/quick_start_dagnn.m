752964ec08fdcbe522998b611c3f1e604240ff25
Minh Nguyen <gordonnguyen3796@gmail.com>
2018-03-16 14:21:33 +0700

    matlab

--- MatConvNet Tutorials/quick_start_dagnn.m
+++ MatConvNet Tutorials/quick_start_dagnn.m
@@ -1,12 +1,12 @@
 % setup MatConvNet
-run  matlab/vl_setupnn
+run ../../matconvnet-1.0-beta25/matlab/vl_setupnn;
 
 % load the pre-trained CNN
-net = dagnn.DagNN.loadobj(load('/media/minhnd/Computer 2/TRINHVANDUY_Thesis/pretrained_models/imagenet-resnet-101-dag.mat')) ;
+net = dagnn.DagNN.loadobj(load('../../pretrained_models/imagenet-resnet-101-dag.mat')) ;
 net.mode = 'test' ;
 
 % load and preprocess an image
-im = imread('/media/minhnd/Computer 2/TRINHVANDUY_Thesis/TRINHVANDUY_CODE/test_images/tank1.jpg') ;
+im = imread('../test_images/tank1.jpg') ;
 im_ = single(im) ; % note: 0-255 range
 im_ = imresize(im_, net.meta.normalization.imageSize(1:2)) ;
 im_ = bsxfun(@minus, im_, net.meta.normalization.averageImage) ;
--- MatConvNet Tutorials/quick_start_simplenn.m
+++ MatConvNet Tutorials/quick_start_simplenn.m
@@ -1,9 +1,12 @@
-run matlab/vl_setupnn;
-net = load('/media/minhnd/Computer 2/TRINHVANDUY_Thesis/pretrained_models/imagenet-vgg-verydeep-19.mat');
+% setup MatConvNet
+run ../../matconvnet-1.0-beta25/matlab/vl_setupnn;
+
+% load the pre-trained CNN
+net = load('../../pretrained_models/imagenet-vgg-verydeep-19.mat');
 net = vl_simplenn_tidy(net);
 
 % Obtain and preprocess an image.
-im = imread('/media/minhnd/Computer 2/TRINHVANDUY_Thesis/TRINHVANDUY_CODE/test_images/dog.jpg') ;
+im = imread('../test_images/dog.jpg') ;
 im_ = single(im); % note: 255 range
 resized_im = imresize(im_, net.meta.normalization.imageSize(1:2), 'AntiAliasing', false);
 im_ = resized_im - net.meta.normalization.averageImage;
