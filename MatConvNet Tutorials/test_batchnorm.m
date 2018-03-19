19abcd43091597037823d1201dd2e6ec8e8bd74d
Minh Nguyen <gordonnguyen3796@gmail.com>
2018-03-19 12:02:33 +0700

    conv bn relu

--- MatConvNet Tutorials/test_batchnorm.m
+++ MatConvNet Tutorials/test_batchnorm.m
@@ -1,20 +1,23 @@
-num_channels = 64;
+num_channels = 256;
+height = 56; width = height;
 
-bn_conv1_mult = net.params(2).value;
-bn_conv1_bias = net.params(3).value;
+bn_conv1_mult = net.params(6).value;
+bn_conv1_bias = net.params(7).value;
 
-bn_conv1_mean = net.params(4).value(:, 1);
-bn_conv1_variance = net.params(4).value(:, 2);
-epsilon = 1e-5 * ones(112, 112, 64);
-
-variance = ones(112, 112, 64);
+bn_conv1_mean = net.params(8).value(:, 1);
+bn_conv1_variance = net.params(8).value(:, 2);
+epsilon = 0 * ones(height, width, num_channels);
+variance = ones(height, width, num_channels);
 mean = variance; bias = variance; mult = variance;
 
-for channel = 1:64
+for channel = 1:num_channels
     mult(:, :, channel) = mult(:, :, channel) * bn_conv1_mult(channel, :);
-    bias(:, :, channel) = bias(:, :, channel) * bn_conv1_bias(channel,:);
-    mean(:, :, channel) = mean(:, :, channel) * bn_conv1_mean(channel,:);
-    variance(:, :, channel) = variance(:, :, channel) * bn_conv1_variance(channel,:);
+    bias(:, :, channel) = bias(:, :, channel) * bn_conv1_bias(channel, :);
+    mean(:, :, channel) = mean(:, :, channel) * bn_conv1_mean(channel, :);
+    variance(:, :, channel) = variance(:, :, channel) * bn_conv1_variance(channel, :);
 end
 
-conv1x = mult .* ((conv1 - mean) ./ sqrt(variance .^2  + epsilon)) + bias;
+bn_tmp = mult .* ((net.vars(6).value - mean) ./ sqrt(variance .^ 2  + epsilon)) + bias;
+bn_tmp2 = vl_nnbnorm(net.vars(6).value, bn_conv1_mult, bn_conv1_bias, 'epsilon', 1e-5, 'moments', net.params(8).value);
+sum(sum(sum(bn_tmp - bn_tmp2)))
+sum(sum(sum(net.vars(7).value - bn_tmp2)))
--- infer_imagenet_resnet_101.py
+++ infer_imagenet_resnet_101.py
@@ -10,7 +10,27 @@ def _input():
     x = tf.placeholder(dtype=tf.float32, shape=[None, 224, 224, 3], name='input')
     return x
 
-def construct_batch_normalisation_block(current, net, weights, weight_start_index, param_names, layer_names):
+def create_branch_layer_names(res_num, res_type, branch_type, branch_order=''):
+    if branch_type == 1 or branch_order == 'c':
+        name_list = [None] * 2
+        name_list[0] = 'res' + str(res_num) + res_type + '_branch' + str(branch_type) + branch_order
+        name_list[1] = 'bn' + str(res_num) + res_type + '_branch' + str(branch_type) + branch_order
+    else:
+        name_list = [None] * 3
+        name_list[0] = 'res' + str(res_num) + res_type + '_branch' + str(branch_type) + branch_order
+        name_list[1] = 'bn' + str(res_num) + res_type + '_branch' + str(branch_type) + branch_order
+        name_list[2] = 'res' + str(res_num) + res_type + '_branch' + str(branch_type) + branch_order + '_relu'
+    return name_list
+
+def create_param_names_from_layers(layer_names):
+    param_list = [None] * 4
+    param_list[0] = layer_names[0] + '_filter'
+    param_list[1] = layer_names[1] + '_mult'
+    param_list[2] = layer_names[1] + '_bias'
+    param_list[3] = layer_names[1] + '_moments'
+    return param_list
+
+def construct_test_batch_normalisation_block(current, net, weights, weight_start_index, param_names, layer_names):
     scale = weights[weight_start_index + 1][1].reshape(-1)
     scale = utils.get_variable(scale, name=param_names[1])
 
@@ -22,75 +42,65 @@ def construct_batch_normalisation_block(current, net, weights, weight_start_inde
 
     variance = weights[weight_start_index + 3][1][:, 1].reshape(-1)
     variance = utils.get_variable(variance, name=param_names[3] + '_variance')
-    variance_epsilon = 1e-5
 
-    current = tf.add(tf.multiply(scale,
-        tf.divide(tf.subtract(current, mean),
-            tf.sqrt(tf.add(tf.square(variance), variance_epsilon)))), offset, name=layer_names[1])
+    current = tf.add(tf.multiply(scale, tf.divide(tf.subtract(current, mean), variance)), offset, name=layer_names[1])
+
     net[layer_names[1]] = current
 
     return current, net
 
-
-def construct_conv1_block(current, net, weights, weight_start_index, param_names, layer_names):
-    # conv1
-    conv1_filter = weights[weight_start_index][1]
-    kernel_size = conv1_filter.shape[0]
-    padding = int((kernel_size - 1) / 2)
-    conv1_filter = utils.get_variable(conv1_filter, name=param_names[0])
-    current = tf.nn.conv2d(
-        tf.pad(current, tf.constant([[0, 0], [padding, padding], [padding, padding], [0, 0]])),
-        conv1_filter, strides=[1, 2, 2, 1],
-        padding="VALID",
-        name=layer_names[0])
+def construct_conv_bn_block(current, net, weights, weight_start_index, param_names, layer_names, stride):
+    # conv
+    kernels = weights[weight_start_index][1]
+    kernels = utils.get_variable(kernels, name=param_names[0])
+    if stride == 1:
+        current = tf.nn.conv2d(current, kernels, strides=[1, 1, 1, 1], padding='SAME', name=layer_names[0])
+    else:
+        kernel_size = kernels.shape[0]
+        padding = int((int(kernel_size) - 1) / 2)
+        current = tf.nn.conv2d(
+            tf.pad(current, tf.constant([[0, 0], [padding, padding], [padding, padding], [0, 0]])),
+            kernels, strides=[1, stride, stride, 1], padding='VALID', name=layer_names[0])
     net[layer_names[0]] = current
 
-    # bn_conv1
-    current, net = construct_batch_normalisation_block(current, net, weights, weight_start_index, param_names, layer_names)
-
-    # conv1_relu
-    current = tf.nn.relu(current, name=layer_names[2])
-    net[layer_names[2]] = current
-
-    # pool1
-    current = tf.nn.max_pool(current, ksize=[1, 3, 3, 1], strides=[1, 2, 2, 1], padding='SAME', name=layer_names[3])
-    net[layer_names[3]] = current
-
+    # bn
+    print(layer_names)
+    print(param_names)
+    print()
+    current, net = construct_test_batch_normalisation_block(current, net, weights, weight_start_index, param_names, layer_names)
     return current, net
 
-def create_branch1_layer_names(res_num):
-    name_list = [None] * 2
-    name_list[0] = 'res' + str(res_num) + 'a' + '_branch1'
-    name_list[1] = 'bn' + str(res_num) + 'a' + '_branch1'
-    return name_list
-
-def create_param_names_from_layers(layer_names):
-    param_list = [None] * 4
-    param_list[0] = layer_names[0] + '_filter'
-    param_list[1] = layer_names[1] + '_mult'
-    param_list[2] = layer_names[1] + '_bias'
-    param_list[3] = layer_names[1] + '_moments'
-    return param_list
-
-def construct_branch1_block(current, net, weights, weight_start_index, branch1_param_names, branch1_layer_names):
-    return None, None
+def construct_conv_bn_relu_block(current, net, weights, weight_start_index, param_names, layer_names, stride):
+    # conv_bn
+    current, net = construct_conv_bn_block(current, net, weights, weight_start_index, param_names, layer_names, stride)
+    # relu
+    current = tf.nn.relu(current, name=layer_names[2])
+    net[layer_names[2]] = current
+    return current, net
 
 def resnet101_net(weights, image):
     net = {}
     current = image
+    param_start_index = 0
 
     # conv1 block
     conv1_layer_names = ['conv1', 'bn_conv1', 'conv1_relu', 'pool1']
     conv1_param_names = create_param_names_from_layers(conv1_layer_names)
-    current, net = construct_conv1_block(current, net, weights, 0, conv1_param_names, conv1_layer_names)
-    pool1 = current
+    current, net = construct_conv_bn_relu_block(current, net, weights, param_start_index, conv1_param_names, conv1_layer_names, 2)
+    current = tf.nn.max_pool(current, ksize=[1, 3, 3, 1], strides=[1, 2, 2, 1], padding='SAME', name=conv1_layer_names[3])
+    net[conv1_layer_names[3]] = current
+    param_start_index += 4
 
-    # res2 block
-    branch1_layer_names = create_branch1_layer_names(2)
+    # res2a_branch1 block
+    branch1_layer_names = create_branch_layer_names(res_num=2, res_type='a', branch_type=1)
     branch1_param_names = create_param_names_from_layers(branch1_layer_names)
-    print(branch1_layer_names)
-    print(branch1_param_names)
-    # current, net = construct_branch1_block(current, net, weights, 4, branch1_param_names, branch1_layer_names)
+    current, net = construct_conv_bn_block(net['pool1'], net, weights, param_start_index, branch1_param_names, branch1_layer_names, 1)
+    param_start_index += 4
+
+    # res2a_branch2 block
+    branch2a_layer_names = create_branch_layer_names(res_num=2, res_type='a', branch_type=2, branch_order='a')
+    branch2a_param_names = create_param_names_from_layers(branch2a_layer_names)
+    current, net = construct_conv_bn_relu_block(net['pool1'], net, weights, param_start_index, branch2a_param_names, branch2a_layer_names, 1)
     return net
 
 def inference(x, weights):
@@ -113,14 +123,13 @@ def main(argv=None):
     resized_img = resize(img, (224, 224), preserve_range=True, mode='reflect')
     normalised_img = utils.process_image(resized_img, mean) """
     
-    with tf.device('/cpu:0'):
-        x = _input()
-        image_net = inference(x, weights)
-        sess = tf.Session(config=tf.ConfigProto(log_device_placement=False))
-        sess.run(tf.global_variables_initializer())
-        # score, category = sess.run([tf.reduce_max(image_net['prob'][0][0][0]), predicted_class],
-        #                             feed_dict={x:normalised_img[np.newaxis, :, :, :].astype(np.float32)})
-        inspected_values = sess.run(image_net['pool1'], feed_dict={x:tmp['im_'][np.newaxis, :, :, :].astype(np.float32)})
+    x = _input()
+    image_net = inference(x, weights)
+    sess = tf.Session()
+    sess.run(tf.global_variables_initializer())
+    # score, category = sess.run([tf.reduce_max(image_net['prob'][0][0][0]), predicted_class],
+    #                             feed_dict={x:normalised_img[np.newaxis, :, :, :].astype(np.float32)})
+    inspected_values = sess.run(image_net['res2a_branch2a_relu'], feed_dict={x:tmp['im_'][np.newaxis, :, :, :].astype(np.float32)})
     print(np.sum(inspected_values))
     print(inspected_values.shape)
     # print('Category:', resnet101_net['classes'][0][0][1][0][category][0])
